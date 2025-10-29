62c0fd12bcd668545955382c071ddcab5e4bec9e
yykgvf8r9n-debug <yykgvf8r9n@privaterelay.appleid.com>
2025-10-29 14:30:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ st.write("here is our first attempt at using data to create a table")
 st.write(pd.DataFrame({
     'first column' : [1, 2, 3, 4],
     'second column' : [10, 20, 30, 40]
-
 }))
 
 st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
-
 all_users = ["v", "vv", "vvv"]
 with st.container(border=True):
     users = st.multiselect("Users", all_users, default=all_users)
