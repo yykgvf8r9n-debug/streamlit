2a7acc558ceabb561fa10f2e5d1a2fa09234028a
yykgvf8r9n-debug <yykgvf8r9n@privaterelay.appleid.com>
2025-10-29 14:33:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title("my first stramlit app")
+
+st.write("here is our first attempt at using data to create a table")
+st.write(pd.DataFrame({
+    'first column' : [1, 2, 3, 4],
+    'second column' : [10, 20, 30, 40]
+}))
+
 st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
 
 all_users = ["Alice", "Bob", "Charly"]
@@ -17,3 +25,4 @@ if rolling_average:
 tab1, tab2 = st.tabs(["Chart", "Dataframe"])
 tab1.line_chart(data, height=250)
 tab2.dataframe(data, height=250, use_container_width=True)
+
